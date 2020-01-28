70239c63fe95f1d1949c1451f01c2faefab4081b
douglassouza1 <douglassouza12-@hotmail.com>
2020-01-28 14:45:59 +0000

    commit2

--- web/src/Componentes/Jumbo/Jumbo.js
+++ web/src/Componentes/Jumbo/Jumbo.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import {Jumbotron, Button} from 'react-bootstrap'
-
+import './Jumbo.css';
 
 // import { Container } from './styles';
 
 export default function Jumbo () {
   return (
-    
-    <Jumbotron>
+ 
+    <Jumbotron className= 'JumboCor'>
   <h1> Bem Vindo a Cadeiras Cars !!!</h1>
   <p>
-    Solucionamos qualquer Problema na Sua cadeira .
+    Solucionamos qualquer Problema na Sua cadeira .<br/>
     Até nas suas Rodinhas!!!
       </p>
   <p>
     <Button variant="primary">Ler Mais</Button>
   </p>
 </Jumbotron>
+
   );
 }
